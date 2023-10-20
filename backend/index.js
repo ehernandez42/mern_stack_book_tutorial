f9c840620d13fbdebcd4cb5bdb11ade89bbb87a9
Eleazar <eleazarfhernandez@gmail.com>
2023-10-19 19:23:46 -0500

    schema created

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 //backend for book store app
-
 const express = require('express');
 const mongoose = require('mongoose');
 
 
-
-
+//mongodb+srv://admin:vuEY6qjU2kdTMmaa@cluster0.cbyw4mp.mongodb.net/?retryWrites=true&w=majority
 
 const app = express();
+
+app.use('/', (req, res, next) => {
+    res.send("connected bruh lol");
+})
+
+mongoose
+    .connect("mongodb+srv://admin:vuEY6qjU2kdTMmaa@cluster0.cbyw4mp.mongodb.net/?retryWrites=true&w=majority"
+)
+    .then (() => console.log("Connected to DB"))
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch((err)=> console.log(err));
 
 
 
