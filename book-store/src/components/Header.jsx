2769273fc3c5725468f05f38a96d6be717c8a02e
Eleazar <eleazarfhernandez@gmail.com>
2023-10-31 18:52:04 -0500

    setState on the floater app navbar; added name to title "Eleazar's Book Store";

--- book-store/src/components/Header.jsx
+++ book-store/src/components/Header.jsx
@@ -2,12 +2,14 @@ import React, {useState} from 'react';
 import {AppBar, Tab, Tabs, Toolbar, Typography} from "@mui/material";
 import CollectionsBookmarkRoundedIcon from '@mui/icons-material/CollectionsBookmarkRounded';
 
+//stopped here!
+//https://www.youtube.com/watch?v=5Y5QKfxTErU&t=2658s
 const bookStoreTitle = " Eleazar's Book Store";
 const Header = () => {
     const [floater, setFloater] = useState();
     return (
     <div>
-        <AppBar position='Sticky'>
+        <AppBar sx={{backgroundColor: "#8f9779"}} position='Sticky'>
             <Toolbar>
             <Typography>
                 <div>
@@ -15,7 +17,10 @@ const Header = () => {
                 {bookStoreTitle}
                 </div>
             </Typography>
-                <Tabs textColor='inherit' indicatorColor='secondary' value={floater} onChange={(e, val) => setFloater(val)}>
+                <Tabs sx={{ml: 'auto'}} textColor='inherit' TabIndicatorProps={{
+                    style: {backgroundColor: "#18392b"}
+                }} value={floater} onChange={(e, val) => setFloater(val)}>
+                    <Tab label='Books' />
                     <Tab label='Add Book' />
                     <Tab label='About us' />
                 </Tabs>
